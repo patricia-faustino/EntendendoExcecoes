21518bb47a041eea782f8ca68dbd0a7f1b3cc539
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-29 12:09:13 -0300

    Lidando com erros

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,17 +10,68 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(7480, 874150);
 
-           // Console.WriteLine(ContaCorrente.TaxaOperacao);
+            int resultado = Metodo();
 
-            Console.WriteLine("Iniciando aplicação...");
-            int numero = 10;
-            int divisor = 0;
-            int resultado = numero / divisor;
-            Console.WriteLine("Fim da aplicação. Tecle enter para sair...");
+            if(resultado == -2)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+            }
 
             Console.ReadLine();
         }
+
+        public static int Metodo()
+        {
+
+            int resultadoDivisao1 = TestaDivisao(2);
+            int resultadoDivisao2 = TestaDivisao(0);
+            if (resultadoDivisao1 == -2)
+            {
+                return -2;
+            }
+            if (resultadoDivisao2 == -2)
+            {
+                return -2;
+            }
+            return 0;
+
+        }
+
+        public static int TestaDivisao(int divisor)
+        {
+
+            int resultado = Dividir(10, divisor);
+
+            if(resultado == -2)
+            {
+                return -2;
+            }
+            if(resultado == -1)
+            {
+                Console.WriteLine("Não é possível fazer divisão por 0");
+            }
+            else
+            {
+                Console.WriteLine($"Resultado da divisão de 10 por {divisor} é {resultado}");
+            }
+
+            return 0;
+            
+        }
+        public static int  Dividir(int numero, int divisor)
+        {
+            if(divisor == 0)
+            {
+                return -1;
+            }
+
+            if(divisor > numero)
+            {
+                return -2;
+            }
+            return numero / divisor;
+        }
+
     }
 }
