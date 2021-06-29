099eebb3448d026666a4977e5ef2fd762f4a10fb
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-29 11:52:38 -0300

    Utilizando repositorio base para praticar com excecoes

---
+++ ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(7480, 874150);
+
+           // Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.WriteLine("Iniciando aplicação...");
+            int numero = 10;
+            int divisor = 0;
+            int resultado = numero / divisor;
+            Console.WriteLine("Fim da aplicação. Tecle enter para sair...");
+
+            Console.ReadLine();
+        }
+    }
+}
