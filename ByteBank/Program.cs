8bf50a4445eebdffe5d8cb43863b3733367df704
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-01 23:01:22 -0300

    Entendendo finally e using

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,25 +13,68 @@ namespace ByteBank
         {
             try
             {
+                CarregarContas();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Catch no método main");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {     
+            // se for utilizar o using deve-se implementar o IDisposable
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            // ---------------------------------------------------------------------------------------
+            //LeitorDeArquivo leitor = null;
+
+            //try
+            //{
+            // leitor = new LeitorDeArquivo("contas.txt");
+
+            //leitor.LerProximaLinha();
+            //leitor.LerProximaLinha();
+            //leitor.LerProximaLinha();
+            //leitor.LerProximaLinha();
+            //leitor.LerProximaLinha();
+
+            //}
+            //finally
+            //{
+            //    //caso não trate o a exceção podemos chamar somente o finally, sendo esse obrigatório
+            //    Console.WriteLine("Executando o finally");
+            //    //é executado sempre independente de ocorrer exceção
+            //    if(leitor != null)
+            //        leitor.Fechar();
+            //}
+
+        }
+        private static void TestaInnerException()
+        {
+            try
+            {
                 ContaCorrente conta1 = new ContaCorrente(4564, 789684);
                 ContaCorrente conta2 = new ContaCorrente(7891, 456794);
 
                 //conta1.Transferir(1000, conta2);
                 conta1.Sacar(1000);
             }
-            catch(OperacaoFinanceiraException e)
+            catch (OperacaoFinanceiraException e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
 
-               /* Console.WriteLine("Informações da INNER EXCEPTION (EXCEÇÃO INTERNA): ");
-                Console.WriteLine(e.InnerException.Message);
-                Console.WriteLine(e.InnerException.StackTrace);*/
+                /* Console.WriteLine("Informações da INNER EXCEPTION (EXCEÇÃO INTERNA): ");
+                 Console.WriteLine(e.InnerException.Message);
+                 Console.WriteLine(e.InnerException.StackTrace);*/
             }
-
-            Console.ReadLine();
         }
-
         public static void Metodo()
         {
 
